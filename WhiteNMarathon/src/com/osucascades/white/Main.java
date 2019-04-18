f534bcb2d987e604de9110ec45153c615b8f6ef7
Natashia White <whitenat@oregonstate.edu>
2019-04-17 20:22:20 -0700

    Skeleton Marathon program added

---
+++ WhiteNMarathon/src/com/osucascades/white/Main.java
@@ -0,0 +1,22 @@
+package com.osucascades.white;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] names = {
+                "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+                "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"
+        };
+
+        int[] times = {
+                341,273,278,329,445,402,388,275,243,334,412,293,299,343,317,265
+        };
+
+        for(int i = 0; i <names.length; ++i) {
+            System.out.println(names[i] + " : " + times[i]);
+        }
+    }
+
+
+}
