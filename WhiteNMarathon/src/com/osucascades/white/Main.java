84d206a9cfb15195123d3d03e3c3017e55175b4c
Natashia White <whitenat@oregonstate.edu>
2019-04-17 20:30:59 -0700

    Fastest runner printing

--- WhiteNMarathon/src/com/osucascades/white/Main.java
+++ WhiteNMarathon/src/com/osucascades/white/Main.java
@@ -13,9 +13,17 @@ public class Main {
                 341,273,278,329,445,402,388,275,243,334,412,293,299,343,317,265
         };
 
+        int fastestTime = 0;
+        int fastestIndex = 0;
+
         for(int i = 0; i <names.length; ++i) {
-            System.out.println(names[i] + " : " + times[i]);
+            if(times[i] > fastestTime){
+                fastestIndex = i;
+                fastestTime = times[i];
+            }
         }
+
+        System.out.println(names[fastestIndex] + " : " + times[fastestIndex]);
     }
 
 
